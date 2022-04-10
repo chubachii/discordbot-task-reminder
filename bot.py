be4a7db11b25d4fe8b694cafa72e6e9d38c0dc82
chubachi-mac <huyu_pc@outlook.jp>
2022-04-10 17:40:56 +0000

    change database from SQLite3 to Postgresql

--- bot.py
+++ bot.py
@@ -1,11 +1,64 @@
 import os 
+import datetime
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-import redis
+import psycopg2
+import pytz
+
+TOKEN = os.environ.get('BOT_TOKEN')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+w_list = ['(月）', '（火）', '（水）', '（木）', '（金）', '（土）', '（日）']
 
 bot = commands.Bot(command_prefix='!')
+dt_now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+
+@tasks.loop(seconds=5)
+async def loop():
+    await bot.wait_until_ready()
+    global dt_now
+    dt_now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+
+@bot.command()
+async def 課題(ctx, title, content, date_md, detail):
+
+    # mm/dd 形式を yyyy-mm-dd に
+    deadline = addYtoMD(date_md)
+
+    # インサート正常終了
+    if insert(title, content, deadline, detail) is None:
+        embed_list = send_tasks(ctx)
+        for embed in embed_list:
+            await ctx.send(embed=embed)
+
+@bot.command()
+async def 表示(ctx):
+    embed_list = send_tasks(ctx)
+    for embed in embed_list:
+        await ctx.send(embed=embed)
+
+@bot.command()
+async def テーブル削除(ctx):
+    with connect_database() as conn:
+        with conn.cursor() as cur:
+            # テーブル削除
+            cur.execute('''DROP TABLE Tasks''')
+
+            # テーブルを作成する SQL を準備
+            sql = '''
+                CREATE TABLE Tasks (
+                    title TEXT,
+                    content TEXT,
+                    deadline DATE,
+                    detail TEXT
+                );
+                '''
+            # SQLを実行
+            cur.execute(sql)
+
+            # コミットし、変更を確定する
+            conn.commit()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -13,27 +66,68 @@ async def on_command_error(ctx, error):
         embed = discord.Embed(title="Error -BadArgument", description="引数が足りません", color=discord.Colour.red())
         await ctx.send(embed=embed, delete_after=5.0) 
 
+def send_tasks(ctx):
+    # 締切の遅い順に日付を取得
+    with connect_database() as conn:
+        with conn.cursor() as cur:
+            deadlines = []
+            cur.execute('SELECT * FROM Tasks ORDER BY deadline DESC')
+            for row in cur:
+                deadlines.append(row[2])
 
-@bot.command()
-async def 課題(ctx, title, content, simple_date, detail):
+            cur.execute('SELECT * FROM Tasks ORDER BY deadline DESC')
+            embed_list = []
+            for i, row in enumerate(cur):
+                deadline = row[2]
+                
+                if deadline.year < dt_now.year or (deadline.month < dt_now.month and (deadline.year != dt_now.year+1)) or (deadline.year == dt_now.year and deadline.month == dt_now.month and deadline.day < dt_now.day):
+                    # 課題なし
+                    if i == 0:
+                        embed_list.append(discord.Embed(title = "課題はありません", color=0xffecd5))
+                        return embed_list
+                    continue 
+                
+                if i == 0 or deadlines[i-1] != deadline:
+                    ## 曜日取得
+                    day_t = deadline.weekday()
+                    embed=discord.Embed(title = str(deadline.month) + ' / ' + str(deadline.day) + w_list[day_t], color=0xffecd5)
 
-    conn = connect_redis()
-    task = conn.set('title', title)
-    result = conn.get('title')
+                embed.add_field(name='・' + row[0] + '  :  ' + row[1] + '\n　詳細  :  ' + row[3], value=" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -", inline=False)
+                
+                if i == len(deadlines)-1 or deadlines[i+1] != deadline:
+                    embed_list.append(embed)    
+            return embed_list
+
+def connect_database():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    return conn
+
+def insert(title, content, deadline, detail):
+
+    try:
+        with connect_database() as conn:
+            with conn.cursor() as cur:
+                # SQLを実行
+                cur.execute('INSERT INTO tasks (title, content, deadline, detail) VALUES (%s, %s, %s, %s)', (title, content, deadline, detail,))
+            
+                # コミットし、変更を確定する
+                conn.commit()
+
+                return None
+        
+    except Exception as ex:
+        return ex
+ 
+def addYtoMD(date_md):
+
+    md = date_md.split('/') # 月と日に分割
+    if dt_now.month > int(md[0]): y = dt_now.year + 1
+    else: y = dt_now.year
+
+    date = str(y) + '-' + str(md[0]) + '-' + str(md[1])  # yyyy-mm-dd形式
+    date_ymd = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    return date_ymd
 
 
-
-
-    await ctx.send(str(result))
-    await ctx.send('a')
-
-
-token = os.environ.get('BOT_TOKEN')
-bot.run(token)
-
-
-def connect_redis():
-    return redis.from_url(
-        url=os.environ.get('REDIS_URL'),
-        decode_responses=True,
-    )
+loop.start()
+bot.run(TOKEN)
