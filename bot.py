39bf5095be11bc36db4c35bcb4edd845646eaa72
chubachi-mac <huyu_pc@outlook.jp>
2022-04-10 07:01:00 +0000

    引数の数を限定

--- bot.py
+++ bot.py
@@ -7,10 +7,7 @@ bot = commands.Bot(command_prefix='!')
 
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def on_command_error(ctx, error): await ctx.send("構文エラー")
 
 
 @bot.command()
