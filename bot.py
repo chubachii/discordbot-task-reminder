32530e8302a4393f41b10997a2655ca2b0af09c9
chubachi-mac <huyu_pc@outlook.jp>
2022-04-10 07:22:20 +0000

    fix error handle

--- bot.py
+++ bot.py
@@ -7,14 +7,24 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.BadArgument):
+    elif isinstance(error, discord.ext.commands.errors.CommandNotFound):
+        embed = discord.Embed(title=":x: 失敗 -CommandNotFound", description=f"不明なコマンドもしくは現在使用不可能なコマンドです。", timestamp=ctx.message.created_at, color=discord.Colour.red())
+        embed.set_footer(text="お困りの場合は、サーバー管理者をメンションしてください。")
+        await ctx.send(embed=embed)
+    elif isinstance(error, discord.ext.commands.errors.MemberNotFound):
+        embed = discord.Embed(title=":x: 失敗 -MemberNotFound", description=f"指定されたメンバーが見つかりません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
+        embed.set_footer(text="お困りの場合は、サーバー管理者をメンションしてください。")
+        await ctx.send(embed=embed)
+    elif isinstance(error, discord.ext.commands.errors.BadArgument):
         embed = discord.Embed(title=":x: 失敗 -BadArgument", description=f"指定された引数がエラーを起こしているため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
         embed.set_footer(text="お困りの場合は、サーバー管理者をメンションしてください。")
-        await ctx.send(embed=embed, delete_after=5.0) 
+        await ctx.send(embed=embed) 
     elif isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
         embed = discord.Embed(title=":x: 失敗 -BadArgument", description=f"指定された引数が足りないため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
         embed.set_footer(text="お困りの場合は、サーバー管理者をメンションしてください。")
-        await ctx.send(embed=embed, delete_after=5.0) 
+        await ctx.send(embed=embed) 
+    else:
+        raise error
 
 
 @bot.command()
