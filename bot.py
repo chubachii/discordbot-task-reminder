3877be0d7c2f3fd32e8c6370b25719f54ad30735
chubachi-mac <huyu_pc@outlook.jp>
2022-04-10 07:45:19 +0000

    test redis

--- bot.py
+++ bot.py
@@ -1,33 +1,37 @@
-from os import getenv
-import traceback
+import os 
 
 import discord
 from discord.ext import commands
+
+import redis
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.BadArgument):
-        embed = discord.Embed(title=":x: 失敗 -BadArgument", description=f"指定された引数がエラーを起こしているため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-        embed.set_footer(text="お困りの場合は、サーバー管理者をメンションしてください。")
+    if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+        embed = discord.Embed(title="Error -BadArgument", description="引数が足りません", color=discord.Colour.red())
         await ctx.send(embed=embed, delete_after=5.0) 
-    elif isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-        embed = discord.Embed(title=":x: 失敗 -BadArgument", description=f"指定された引数が足りないため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-        embed.set_footer(text="お困りの場合は、サーバー管理者をメンションしてください。")
-        await ctx.send(embed=embed, delete_after=3.0) 
-    else:
-        raise error
 
 
 @bot.command()
 async def 課題(ctx, title, content, simple_date, detail):
 
+    conn = connect_redis()
+    task = conn.set('title', title)
 
 
 
-    await ctx.send(str(title)+str(content)+str(simple_date))
+
+    await ctx.send(str(task))
 
 
-token = getenv('BOT_TOKEN')
+token = os.environ.get('BOT_TOKEN')
 bot.run(token)
+
+
+def connect_redis():
+    return redis.from_url(
+        url=os.environ.get('REDIS_URL'),
+        decode_responses=True,
+    )
