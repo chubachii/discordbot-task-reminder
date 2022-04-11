1e30833a81af0f0a4bdf60b17e12e5033716d0ef
chubachi-mac <huyu_pc@outlook.jp>
2022-04-11 05:38:59 +0000

    add status message

--- bot.py
+++ bot.py
@@ -9,16 +9,15 @@ import pytz
 
 TOKEN = os.environ.get('BOT_TOKEN')
 DATABASE_URL = os.environ.get('DATABASE_URL')
-REMIND_CHANNEL_ID = '371462166097690627'
-REMIND_TIME = '12:59'
+
+REMIND_CHANNEL_ID = 371462166097690627
+REMIND_TIME = '20:00'
 TIME_ZONE = pytz.timezone('Asia/Tokyo')
 w_list = ['(月）', '（火）', '（水）', '（木）', '（金）', '（土）', '（日）']
 
 
 bot = commands.Bot(command_prefix='!')
 dt_now = datetime.datetime.now()
-client = discord.Client()
-channel_remind = client.get_channel(REMIND_CHANNEL_ID)
 
 @tasks.loop(seconds=5)
 async def loop():
@@ -26,35 +25,23 @@ async def loop():
     global dt_now
     dt_now = datetime.datetime.now(TIME_ZONE)
 
+    # リマインダー
     if dt_now.strftime('%H:%M') == REMIND_TIME:
         embed_list = get_tommorw_tasks()
-        for embed in embed_list:
-            await channel_remind.send(embed=embed)
-
-def get_tommorw_tasks():
-
-    tomorrow = dt_now + datetime.timedelta(1)
-
-    with connect_database() as conn:
-        with conn.cursor() as cur:
-            cur.execute('SELECT * FROM Tasks WHERE deadline = %s', (tomorrow,))
-
-            embed_list = []
-            for i, row in enumerate(cur):
-
-                deadline = row[2]
-               
-                if i == 0:
-                    ## 曜日取得
-                    day_t = deadline.weekday()
-                    embed=discord.Embed(title = str(deadline.month) + ' / ' + str(deadline.day) + w_list[day_t], color=0xffecd5)
-
-                embed.add_field(name='・' + row[0] + '  :  ' + row[1] + '\n　詳細  :  ' + row[3], value=" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -", inline=False)
-                
-                if i == len(cur)-1:
-                    embed_list.append(embed)
-                    return embed_list
-
+        if not embed_list == None:
+            for embed in embed_list:
+                channel_remind = bot.get_channel(REMIND_CHANNEL_ID)
+                await channel_remind.send(embed=embed)
+            
+            
+    # ステータスメッセージ変更
+    if dt_now.strftime('%H:%M') == "00:00":
+        n, text = get_today_tasks()
+        
+        if n > 0:
+            await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=text))
+        else:
+            await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name=text))
 
 @bot.command()
 async def 追加(ctx, title, content, date_md, detail='なし'):
@@ -67,7 +54,14 @@ async def 追加(ctx, title, content, date_md, detail='なし'):
         embed_list = send_tasks(ctx)
         for embed in embed_list:
             await ctx.send(embed=embed)
-
+    
+    # ステータスメッセージ更新
+    n, text = get_today_tasks()
+    if n > 0:
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=text))
+    else:
+        await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name=text))
+        
 @bot.command()
 async def 削除(ctx, title, date_md):
     # mm/dd 形式を yyyy-mm-dd に
@@ -90,6 +84,13 @@ async def 削除(ctx, title, date_md):
     except Exception as ex:
         print(ex)
 
+    # ステータスメッセージ更新
+    n, text = get_today_tasks()
+    if n > 0:
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=text))
+    else:
+        await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name=text))
+       
 @bot.command()
 async def 表示(ctx):
     embed_list = send_tasks(ctx)
@@ -186,6 +187,56 @@ def addYtoMD(date_md):
     date_ymd = datetime.datetime.strptime(date, '%Y-%m-%d').date()
     return date_ymd
 
+def get_tommorw_tasks():
+
+    t = dt_now + datetime.timedelta(1)
+    tomorrow = str(t.year) + '-' + str(t.month) + '-' + str(t.day)  # yyyy-mm-dd形式
+
+    with connect_database() as conn:
+        with conn.cursor() as cur:
+
+            tasks = []
+            cur.execute('SELECT * FROM Tasks WHERE deadline = %s', (tomorrow,))
+            for row in (cur):
+                tasks.append(row)
+
+            if len(tasks) != 0:
+                embed_list = []
+                for i, row in enumerate(tasks):
+                    deadline = row[2]
+                
+                    if i == 0:
+                        ## 曜日取得
+                        day_t = deadline.weekday()
+                        embed=discord.Embed(title = "【 リマインド 】 明日が締切の課題があります", description = str(deadline.month) + ' / ' + str(deadline.day) + w_list[day_t], color=0xff0000)
+
+                    embed.add_field(name='・' + row[0] + '  :  ' + row[1] + '\n　詳細  :  ' + row[3], value=" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -", inline=False)
+                    
+                    if i == len(tasks)-1:
+                        embed_list.append(embed)
+                        return embed_list
+
+            else: return None
+
+def get_today_tasks():
+    date = str(dt_now.year) + '-' + str(dt_now.month) + '-' + str(dt_now.day)  # yyyy-mm-dd形式
+    date_ymd = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+    text = "エラー"
+    n = 0
+    with connect_database() as conn:
+        with conn.cursor() as cur:
+
+            tasks = []
+            cur.execute('SELECT * FROM Tasks WHERE deadline = %s', (date,))
+            for row in (cur):
+                tasks.append(row)
+
+            n = len(tasks)
+
+            text = "今日 " + w_list[date_ymd.weekday()] + "が締切の課題 : " + str(n)
+
+    return n, text
 
 loop.start()
 bot.run(TOKEN)
